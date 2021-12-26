428e48c4975e43bc0e68bee41b15b67c09502983
Yemi Gabriel <yemigabriel@gmail.com>
2021-12-26 10:08:13 +0100

    Initial Commit

---
+++ Etched/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Etched
+//
+//  Created by Yemi Gabriel on 12/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Etched/EtchedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EtchedApp.swift
+//  Etched
+//
+//  Created by Yemi Gabriel on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct EtchedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
